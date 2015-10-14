27de118a118bf7e3acc3de8829ff4355a1b18d9c
QuidditchFIsh <nye42@live.co.uk>
2015-10-14 19:41:16 +0100

    fixed xPos@

--- Function/Position/YPos.m
+++ Function/Position/YPos.m
@@ -1,6 +1,9 @@
-function [ val ] = YPos(  f,z,e,poleVec, semiAxis  )
+function [ val ] = YPos(  f,z,e,poleVec, semiAxis , mu, Mp )
 %YPOS The position of y as a function of f(true anomoaly) and z
-val = 1 + z.*(1 + e.*cos(f)).*(-poleVec(1).*sin(f) + poleVec(2).*cos(f))./(poleVec(3).*semiAxis.*(1 - e.^2));
+%this function will return a unitless quantity. Pole vector needs to have
+%units of length.
+muDim = mu./Mp;
+val = muDim + z.*(1 + e.*cos(f)).*(-poleVec(1).*sin(f) + poleVec(2).*cos(f))./(poleVec(3).*semiAxis.*(1 - e.^2));
 
 end
 
